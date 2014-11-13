c6839e0ed291151ebfef7899e5d9d1d93eed6e5e
Charlie Guthrie <charlesdguthrie@gmail.com>
2014-11-13 18:26:30 -0500

    Removed full_labeled_essays from git.  Too cumbersome

--- scripts/join_essays_to_labels.py
+++ scripts/join_essays_to_labels.py
@@ -1,43 +1,46 @@
 #join_essays_to_labels
 import pandas as pd
+import csv
 
-homedir = '/Users/205341/Documents/git/frontrow/'
-labeled_inpath = homedir + 'essays_and_labels.csv'
-full_essays_path = homedir + 'opendata_essays_1000.csv'
-my_outpath = homedir + 'full_labeled_essays.csv'
+
+labeled_inpath = '../data/essays_and_labels.csv'
+full_essays_path = '../data/opendata_essays.csv'
+my_outpath = '../data/full_labeled_essays.csv'
 
 print "loading " + labeled_inpath
 labeled_df=pd.read_csv(labeled_inpath,sep=',', header=0)
 print "finished loading " + labeled_inpath
-labeled_df["full_essay"]=""
-
 
 def prep_data(inpath,outpath):
-    import csv
 
     #initialize csv file
     with open(outpath, 'wb') as csvfile:
         writer = csv.writer(csvfile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(['got_posted','_projectid','_teacherid','title','essay','need_statement','created_date','full_essay'])
+        writer.writerow(['got_posted','_projectid','_teacherid','title','essay','need_statement','created_date'])
 
     #read lines one at a time
     with open(inpath, 'rb') as inf:
-        csvreader = csv.reader(inf,delimiter=',', quotechar='"')
+        reader = pd.read_csv(inf, delimiter=',', quotechar='"',iterator=True, chunksize=100)
         
         #Read and print header
-        header = inf.readline().split(',')
+        #header = inf.readline().split(',')
 
         #Read and process other lines
-        for row in csvreader:
-            prep_line(header,row)
+        for chunk in reader:
+            m,n = chunk.shape
+            for i in range(m):
+                row = chunk.irow(i)
+                prep_line(row)
+                           
+        #for row in csvreader:
+        #    prep_line(header,row)
             
 
 #process one line.  Produces a dict with key:header, value: data   
-def prep_line(header,row):
+def prep_line(row):
     row[0] = row[0].replace('"', '').strip()
-    line_dict = dict(zip(header,row))
-    finished_line = join_essays_to_labels(line_dict['_projectid'],line_dict['essay'])
-    write_line(my_outpath, finished_line)
+    finished_line = join_essays_to_labels(row['_projectid'],row['essay'])
+    write_line(my_outpath, finished_line.iloc[:,:6])
     '''
     -----------------------------------
     Insert text processing modules here, eg:
@@ -53,10 +56,12 @@ def write_line(outpath,outrow):
         outrow.to_csv(f, header=False, index=False)   
         
 def join_essays_to_labels(pid, essay):
-    labeled_df['full_essay'][labeled_df['_projectid']==pid] = essay
-    if len(labeled_df['full_essay'][labeled_df['_projectid']==pid])>0:
-        print "got a match!" + pid
-    return labeled_df[labeled_df['_projectid']==pid]
+
+    labeled_df['essay'][labeled_df['_projectid']==pid] = essay
+    myline = labeled_df[labeled_df['_projectid']==pid
+    if len(myline)>7:
+        print myline
+    return myline
 
 prep_data(full_essays_path,my_outpath)
 
