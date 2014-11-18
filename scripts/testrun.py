0f817fbcf21710c52add568700eb1376f3788d53
Justin <justinmaojones@gmail.com>
2014-11-17 23:11:56 -0500

    ready for testing on CIMS

--- scripts/testrun.py
+++ scripts/testrun.py
@@ -14,15 +14,22 @@ from nltk.corpus import stopwords
 from collections import defaultdict
 from sklearn.cross_validation import train_test_split
 from stemming import stemming
+import time
 
-file_essays = "full_labeled_essays_1000.csv"
-file_essays_labels = "full_labeled_essays.csv"
-file_resources = "opendata_resources.csv"
+mydir = os.path.dirname(os.path.realpath(__file__))
+pardir = os.path.join(mydir,"..")
+datadir = os.path.join(pardir,"data")
+
+file_essays = os.path.join(datadir,"full_labeled_essays_1000.csv")
+file_essays_labels = os.path.join(datadir,"full_labeled_essays.csv")
+file_essays_labels2 = os.path.join(datadir,"essays_and_labels.csv")
+file_resources = os.path.join(datadir,"opendata_resources.csv")
 
 chunksize = 10000
-essays = pd.read_csv(file_essays,iterator=True,chunksize=chunksize)
-essays_labels = pd.read_csv(file_essays_labels,iterator=True,chunksize=chunksize)
-resources = pd.read_csv(file_resources,iterator=True,chunksize=chunksize)
+#essays_labels = pd.read_csv(file_essays,iterator=True,chunksize=chunksize)
+#essays_labels = pd.read_csv(file_essays_labels,iterator=True,chunksize=chunksize)
+essays_labels = pd.read_csv(file_essays_labels2,iterator=True,chunksize=chunksize)
+#resources = pd.read_csv(file_resources,iterator=True,chunksize=chunksize)
 
 '''
 
@@ -41,6 +48,30 @@ def process_label(label):
         return True
     else:
         return False
+
+def RunItTimeIt(func, args=[], returnarg=False, msg=""):
+    # This just runs the function func with args and prints a message
+    # about how long it took.  You can attach a little message that
+    # goes with it.  For example, "Function X completed in [5 seconds]".
+    # If no return output required, set returnarg to false, otherwise
+    # it will return the output of func.
+    #
+    # NOTE: args must be a list
+    start = time.time()
+    if returnarg == True:
+        if len(args)=0:
+            outargs = func()
+        else:
+            outargs = func(*args)
+    else:
+        if len(args)=0:
+            func()
+        else:
+            func(*args)
+    end = time.time()
+    print msg, round(end-start,2),"seconds"
+    if returnarg == True:
+        return outargs
 
 def generate_features(df):
     # note, this will assign the same label input to all features
@@ -106,26 +137,38 @@ mytest_size = 0.3
 
 features_labels = []
 
-#for each chunk in essays:
 
-#chunk = essays.get_chunk(chunksize)
 
-#headers = essays_labels.get_chunk(1).columns
+# Generate the data frame by first creating creating an empty data frame
+# and successively append each chunk to it.  To get the headers, grab the
+# column names from a chunk of size 1.
+firstchunk = essays_labels.get_chunk(1)
+headers = firstchunk.columns
 
 data_app = pd.DataFrame(columns=headers)
 data_rej = pd.DataFrame(columns=headers)
 
+# breakme is used to stop it from iterating through all the chunks when only
+# small batches sizes are wanted for testing.
+breakme = True
+j = 0
 for chunk in essays_labels:
     data_app = data_app.append(chunk[chunk.got_posted=='t'])
     data_rej = data_rej.append(chunk[chunk.got_posted!='t'])
+    j=j+1
+    if breakme and j >= 5:
+        break
 
+# <<<<< MAY NEED TO BE REVISED IN FUTURE >>>>>
+# for now, just get rid of rows with missing labels.  there aren't that
+# many anyway.
 data_app = data_app[data_app.got_posted.isnull()==False]
 data_rej = data_rej[data_rej.got_posted.isnull()==False]
 
-headers = data_app.columns
+# this is redundant, but for some reason it's needed
+#headers = data_app.columns
 
-data_app = data_app.iloc[-1000:,:]
-data_rej = data_rej.iloc[-1000:,:]
+
 
 data_app = data_app.replace(to_replace={'got_posted':{'t':1,'f':0}})
 data_rej = data_rej.replace(to_replace={'got_posted':{'t':1,'f':0}})
@@ -139,20 +182,22 @@ data_rej_train = pd.DataFrame(data_rej_train,columns=headers)
 data_rej_test = pd.DataFrame(data_rej_test,columns=headers)
 
 
+train_app = RunItTimeIt(generate_features,[data_app_train],True,
+       "finished generating features: training approved,")
+test_app = RunItTimeIt(generate_features,[data_app_test],True,
+       "finished generating features: test approved,")
+train_rej = RunItTimeIt(generate_features,[data_rej_train],True,
+       "finished generating features: training rejected,")
+test_rej = RunItTimeIt(generate_features,[data_rej_test],True,
+       "finished generating features: test rejected,")
 
-train_app = generate_features(data_app_train)
-print "finished generating features: training approved"
-test_app = generate_features(data_app_test)
-print "finished generating features: test approved"
-train_rej = generate_features(data_rej_train)
-print "finished generating features: training rejected"
-test_rej = generate_features(data_rej_test)
-print "finished generating features: test rejected"
 
 train = train_app + train_rej
 test = test_app + test_rej
 
-classifier = NaiveBayesClassifier.train(train)
+
+classifier = RunItTimeIt(NaiveBayesClassifier.train,[train],True,
+       "Train Naive Bayes Classifier,")
 
 print ('Test approved accuracy: {0:.2f}%'
        .format(100 * nltk.classify.accuracy(classifier, test_app)))
