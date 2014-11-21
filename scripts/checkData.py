80a385ae409162bbf22a2dd07b57a8882072a6af
charlesdguthrie <charlesdguthrie@gmail.com>
2014-11-21 15:25:48 -0500

    Fixed check data.  It checks the number of t's, f's, 1's or 0's in got_posted

--- scripts/checkData.py
+++ scripts/checkData.py
@@ -1,8 +1,17 @@
 import pandas as pd
 from sys import argv
+from DataLoading import *
 
 script, second = argv
 data = '../data/' + second
 
-df=pd.read_csv(data,sep=',', header=0)
-print df.got_posted.value_counts()
+freqs = {'t':0, 'f':0, 1:0, 0:0}
+
+def quickSummary(chunk,chunk_id):
+    for i, key_i in enumerate(chunk.got_posted.value_counts().keys()):
+        freqs[key_i] = freqs[key_i] + chunk.got_posted.value_counts()[key_i]
+    print freqs
+    return freqs
+
+chunker(data,quickSummary,10000)
+print freqs
