b0f4f9d8c2513b67c2b6cd45d8a05abd68a81d19
Justin <justinmaojones@gmail.com>
2014-12-11 00:47:05 -0500

    added grid search

--- scripts/working_JMJ2.py
+++ scripts/working_JMJ2.py
@@ -3,7 +3,6 @@ import numpy as np
 import scipy as sp
 
 import Statistics as st
-import TextProcessing as txtpr
 import DataSets as ds
 import FeatureGeneration as fg
 import DataLoading as dl
@@ -26,7 +25,7 @@ binary_col_selector = summary.distinct_count == 2
 nonbinary_col_selector = summary.distinct_count > 2
 binary_cols = dense_df.loc[:,binary_col_selector]
 nonbinary_cols = dense_df.loc[:,nonbinary_col_selector]
-normalized = pd.DataFrame(normalize(nonbinary_cols,norm='l1'),columns=nonbinary_cols.columns)
+normalized = pd.DataFrame(normalize(nonbinary_cols,norm='l2'),columns=nonbinary_cols.columns)
 dense_normalized = pd.concat((binary_cols,normalized),axis=1,ignore_index=True)
 
 # COMBINE ALL FEATURES
@@ -54,11 +53,13 @@ y_train = np.array(rejected[sel_bool_train]).astype(int)
 y_test = np.array(rejected[sel_bool_test]).astype(int)
 
 # CLASSIFIERS
+'''
 clf1 = MultinomialNB().fit(f_train, y_train)
 probs = clf1.predict_proba(f_test)
 fpr,tpr,_ = roc_curve(y_true=y_test,y_score=probs[:,1])
 roc_auc = auc(fpr,tpr)
 st.plotROC(fpr,tpr,roc_auc,"MultinomialNB")
+'''
 
 clf2 = LogisticRegression(penalty='l1').fit(f_train, y_train)
 probs = clf2.predict_proba(f_test)
@@ -66,12 +67,13 @@ fpr,tpr,_ = roc_curve(y_true=y_test,y_score=probs[:,1])
 roc_auc = auc(fpr,tpr)
 st.plotROC(fpr,tpr,roc_auc,"LogReg")
 
+'''
 clf3 = SGDClassifier(penalty='l1').fit(f_train, y_train)
 probs = clf3.decision_function(f_test)
 fpr,tpr,_ = roc_curve(y_true=y_test,y_score=probs)
 roc_auc = auc(fpr,tpr)
 st.plotROC(fpr,tpr,roc_auc,"LogRegSGD")
-
+'''
 
 coef_dense = clf2.coef_[0][selector_dense]
 coef_binary = st.coef_dataframe(
@@ -85,4 +87,45 @@ coef_numerical = st.coef_dataframe(
 coef_sparse = st.coef_dataframe(
                     clf2.coef_[0][selector_sparse],
                     sparseheaders)
-                    
+
+def LogisticGridSearch():  
+    # C=1 is best
+    cs = 10.0**np.arange(-1,2,0.25)
+    aucs = []
+    for c in cs:
+        clf = LogisticRegression(penalty='l1',C=c).fit(f_train, y_train)
+        probs = clf.predict_proba(f_test)
+        fpr,tpr,_ = roc_curve(y_true=y_test,y_score=probs[:,1])
+        roc_auc = auc(fpr,tpr)
+        cstr = '%0.2e'%c
+        st.plotROC(fpr,tpr,roc_auc,"LogReg C="+cstr)
+        aucs.append(roc_auc)
+    return clf
+
+def SGDGridSearch():  
+    # C=1 is best
+    cs = 10.0**np.arange(-9,3,1)
+    aucs = []
+    for c in cs:
+        clf = SGDClassifier(penalty='l2',alpha=c).fit(f_train, y_train)
+        probs = clf.decision_function(f_test)
+        fpr,tpr,_ = roc_curve(y_true=y_test,y_score=probs)
+        roc_auc = auc(fpr,tpr)
+        cstr = '%0.2e'%c
+        st.plotROC(fpr,tpr,roc_auc,"LogReg C="+cstr)
+        aucs.append(roc_auc)
+    return clf
+    
+def NaiveBayesGridSearch():  
+    # C=1 is best
+    cs = 10.0**np.arange(-1,2,0.25)
+    aucs = []
+    for c in cs:
+        clf = MultinomialNB(penalty='l1',C=c).fit(f_train, y_train)
+        probs = clf.predict_proba(f_test)
+        fpr,tpr,_ = roc_curve(y_true=y_test,y_score=probs[:,1])
+        roc_auc = auc(fpr,tpr)
+        cstr = '%0.2e'%c
+        st.plotROC(fpr,tpr,roc_auc,"LogReg C="+cstr)
+        aucs.append(roc_auc)
+    return clf
